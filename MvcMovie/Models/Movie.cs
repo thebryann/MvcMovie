0a196318aaff4538d69f0cca11c7150b5eec2d0a
Bryann Henrique <bryann.enrique@gmail.com>
2021-03-04 02:14:31 -0300

    Init

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Genre { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public decimal Price { get; set; }
+    }
+
+    public class MovieDbContext : DbContext
+    {
+        public DbSet<Movie> movie { get; set; }
+    }
+}
